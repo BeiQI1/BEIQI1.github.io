ddd45f380555def90fde38be7e7067e25d19c6ee
“BEIQI1” <2462171285@qq.com>
2025-01-08 10:24:50 +0800

    Site updated: 2025-01-08 10:24:50

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/01/06/hello-world/"];function toRandomPost(){
+var posts=["2025/01/06/hello-world/","2025/01/08/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
